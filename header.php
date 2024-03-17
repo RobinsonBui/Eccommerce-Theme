33bdb1efa68938821304daf0eaa83176b1c3ea57
Robinson Buitrago <buitragorobinson33@gmail.com>
2024-03-16 20:24:59 -0500

    new seting in tag manager

--- header.php
+++ header.php
@@ -19,25 +19,11 @@
 <body <?php body_class(); ?>>
     <?php
     wp_body_open();
-    echo stripslashes(get_option('field_body', ''));
-    // $options_fields =  get_fields('option');
+    echo stripslashes(get_option('option_body', ''));
     ?>
     <div id="page">
         <header id="masthead" class="site-header header">
             <div class="header__wrapper max-width">
-                <!-- <a href="/">
-                    <figure class="header__branding">
-                        < ?php
-                        if (function_exists('the_custom_logo')) {
-                            $custom_logo_id = get_theme_mod('custom_logo');
-                            if ($custom_logo_id) {
-                                $custom_logo_url = wp_get_attachment_image_url($custom_logo_id, 'full');
-                                echo '<img src="' . esc_url($custom_logo_url) . '" alt="' . wp_title('') . '">';
-                            }
-                        }
-                        ?>
-                    </figure>
-                </a> -->
                 <?php the_custom_logo() ?>
                 <nav class="header__navigation" id="primaryNavigation">
                     <?php
