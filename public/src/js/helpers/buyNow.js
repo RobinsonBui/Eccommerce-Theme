be336dac0f5ce88d32fdad901bb7378187c7932b
Robinson Buitrago <buitragorobinson33@gmail.com>
2024-04-07 16:29:08 -0500

    correction functions in page filter

--- public/src/js/helpers/buyNow.js
+++ public/src/js/helpers/buyNow.js
@@ -4,13 +4,12 @@ if (cards) {
     buyNowButton.forEach(button => {
         button.addEventListener('click', function (event) {
             event.preventDefault();
-            console.log(button)
             const productId = this.dataset.productId;
             let addToCartButton = document.querySelector('.cart [type="submit"]');
             addToCartButton.click();
 
             setTimeout(function () {
-                window.location.href = `http://ecoommerce.local/checkout/?add-to-cart=${productId}`;
+                window.location.href = `/checkout/?add-to-cart=${productId}`;
             }, 500);
         });
     });
